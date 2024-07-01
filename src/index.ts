9ddb8c8c805a5c1d4a859edd7f0d2b7ef88605ac
windbullet <2536810643@qq.com>
2024-07-01 10:08:06 +0800

    更新

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, Random } from 'koishi'
 
 export const name = 'choice'
 
-export interface Config {}
+export interface Config {
+  content: string
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.object({
+  content: Schema.string()
+    .description("选择后的回复内容（$choice 代表抽取的选择）")
+    .default("我觉得$choice不错"),
+})
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export function apply(ctx: Context, config: Config) {
+  ctx.command("choose <...choices>")
+    .alias("帮我选一个")
+    .action(async ({ session }, ...choices) => {
+      return config.content.replaceAll("$choice", Random.pick(choices))
+    })
 }
